ff697c1dbff10c0adbd324f962c591d287e25534
RodyMan <61457485+RodyMan@users.noreply.github.com>
2022-09-15 07:34:27 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="Foundationlogo.png">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title> LinkTunnel | PC </title>
+</head>
+<body>
+	
+	<iframe src="https://www.addictinggames.com/embed/html5-games/20543" scrolling="no"></iframe>
+
+</body>
+</html>
